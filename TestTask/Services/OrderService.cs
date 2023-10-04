bad12d73ec0326672e99ea9423b0198f776e5907
sashamed <mednissasha@gmail.com>
2023-10-04 14:56:27 +0300

    initial commit

---
+++ TestTask/Services/OrderService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class OrderService : IOrderService
+    {
+
+        private const int thresholdQuantity = 10;
+        private ApplicationDbContext applicationDbContext;
+
+        public OrderService(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<Order> GetOrder()
+        {
+            var order = await applicationDbContext.Orders.OrderByDescending(o => o.Price * o.Quantity) .FirstOrDefaultAsync(); 
+            return order;
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            var orders = await applicationDbContext.Orders.Where(c => c.Quantity > thresholdQuantity).ToListAsync();
+            return orders;
+        }
+    }
+}
---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class UserService : IUserService
+    {
+
+        private ApplicationDbContext applicationDbContext;
+
+        public UserService(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<User> GetUser()
+        {
+            var user = applicationDbContext.Users.OrderByDescending(u => u.Orders.Count).FirstOrDefault();
+            return user;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            var users = await applicationDbContext.Users.Where(c => c.Status == Enums.UserStatus.Inactive).ToListAsync();
+            return users;
+        }
+    }
+}
